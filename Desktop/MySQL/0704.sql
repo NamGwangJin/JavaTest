a6141b00a92cd7d2281ffb3b71511e33925d8b12
[NamGwangJin] <[n45202605@gmail.com]>
2023-07-04 12:33:45 +0900

    [test]

--- Desktop/MySQL/0704.sql
+++ Desktop/MySQL/0704.sql
@@ -1,6 +1,16 @@
 show tables;
 select * from employees;
-select * from departments;
+select * from jobs;
 
 select e.employee_id 사번, e.emp_name 이름, d.department_name 부서
-from employees e join departments d on e.department_id = d.department_id;
+from employees e join departments d on d.department_id=e.DEPARTMENT_ID;
+
+select e.EMPLOYEE_ID 사번, e.EMP_NAME 이름, j.job_title 직무
+from employees e join jobs j on j.job_id = e.job_id
+order by 직무;
+
+select e.employee_id 사번, e.emp_name 이름, d.department_name 부서, j.job_title 직무
+from employees e join departments d on d.department_id=e.DEPARTMENT_ID
+				 join jobs j on j.job_id = e.job_id
+order by 부서;
+
